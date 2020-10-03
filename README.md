0235065041e07c3ee6da03021bd9a90453ae2e52
Anton Holmberg <anton@antonholmberg.se>
2020-10-03 23:59:27 +0200

    Iniital doc an WIP lexer

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-kaleidoscope-compiler
+# Kaleidoscope Compiler
+
+This is a personal project trying to implement a kaleidoscope compiler in
+rust. This is by no means intended for anything other than educational
+purposes for myself.
+
+## How It's Intended To Work
+
+I aim to have a number of components. These components will their own
+individual rust packages.
+
+### Lexer
+
+Breaks a string into tokens.
+
+**Status:** In Progress.
+
+### Parser
+
+Converts the tokens provided by the _Lexer_ into a Abstract Syntax Tree
+(AST).
+
+**Status:** Not Started.
+
+### Codegen
+
+Converts the AST provided by the _Parser_ into LLVM Intermediate
+Representation (IR).
+
+**Status:** Not Started.
+
+### Runtime
+
+Not sure if this is needed yet. But if needed it would provide some
+externally callable functions that our output program can be linked to in
+order to use.
+
+**Status:** Not Started.
+
+### Compiler
+
+Binds all other rust packages into an executable that someone can run. In addition to this is will handle things like:
+
+- Reading files.
+- Link to potential runtime.
+- Outputting the final executable.
+
+**Status:** Not Started.
+
+## Useful Links
+
+I try to follow [this](https://llvm.org/docs/tutorial/) tutorial the best I
+can. It is written in C++ but the basic principals are the same.
---
+++ lexer/src/lib.rs
@@ -0,0 +1,143 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
+#[derive(PartialEq, Debug)]
+pub enum Token<'a> {
+    Def,
+    Extern,
+    Identifier(&'a str),
+    Number(f64),
+    GroupL,
+    GroupR,
+    Separator,
+}
+
+pub struct ParseTokenError();
+
+pub struct Lexer<'a> {
+    pub source: &'a str,
+}
+
+pub struct LexerIterator<'a> {
+    source: &'a str,
+    start: usize,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(source: &'a str) -> Self {
+        Lexer { source: source }
+    }
+
+    pub fn iter(&self) -> LexerIterator<'a> {
+        LexerIterator {
+            source: self.source,
+            start: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for LexerIterator<'a> {
+    type Item = Token<'a>;
+    fn next(&mut self) -> Option<Token<'a>> {
+        let mut offset = 0;
+        let token = loop {
+            offset += 1;
+
+            if self.start + offset > self.source.len() {
+                break None;
+            }
+
+            let current = &self.source[self.start..(self.start + offset)];
+            let next = self.source.chars().nth(self.start + offset);
+
+            match current {
+                "(" => break Some(Token::GroupL),
+                ")" => break Some(Token::GroupR),
+                "," => break Some(Token::Separator),
+                " " | "\n" => {
+                    self.start += 1;
+                    offset -= 1;
+                    continue;
+                }
+                _ => match next {
+                    Some(' ') | Some(')') | Some('(') | Some(',') | Some('\n') | None => {
+                        if current == "extern" {
+                            break Some(Token::Extern);
+                        }
+                        if current == "def" {
+                            break Some(Token::Def);
+                        }
+
+                        if let Ok(num) = current.parse::<f64>() {
+                            break Some(Token::Number(num));
+                        }
+
+                        break Some(Token::Identifier(current));
+                    }
+                    _ => continue,
+                },
+            };
+        };
+
+        self.start += offset;
+
+        return token;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Lexer, Token};
+    #[test]
+    fn basic_lexing() {
+        let input = "def hello";
+        let lexer = Lexer::new(input);
+
+        let output = lexer.iter().collect::<Vec<Token>>();
+
+        assert_eq!(vec![Token::Def, Token::Identifier("hello")], output);
+    }
+
+    #[test]
+    fn argument_lexing() {
+        let input = "def hello(x, y)";
+        let lexer = Lexer::new(&input);
+
+        let output = lexer.iter().collect::<Vec<Token>>();
+
+        assert_eq!(
+            vec![
+                Token::Def,
+                Token::Identifier("hello"),
+                Token::GroupL,
+                Token::Identifier("x"),
+                Token::Separator,
+                Token::Identifier("y"),
+                Token::GroupR
+            ],
+            output
+        )
+    }
+
+    #[test]
+    fn junk_code() {
+        let input = "(def hello(x, y)";
+        let lexer = Lexer::new(input);
+
+        let output = lexer.iter().collect::<Vec<Token>>();
+
+        assert_eq!(
+            vec![
+                Token::GroupL,
+                Token::Def,
+                Token::Identifier("hello"),
+                Token::GroupL,
+                Token::Identifier("x"),
+                Token::Separator,
+                Token::Identifier("y"),
+                Token::GroupR
+            ],
+            output
+        )
+    }
+}
